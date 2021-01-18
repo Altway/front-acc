5392165b12c8bd5a28b481ecb71bd0f09965c4c9
Rems <remi.gazquez@gmail.com>
2021-01-18 20:04:54 +0100

    Clean data missing update

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="row">
-    <!-- Big Chart -->
     <div class="col-12">
       <card type="chart">
         <template slot="header">
-          <base-input type="=id" label="hypotethis_id" placeholder="hypotethis_id" v-model="hypotethis_id"/>
+          <base-input type="=id" label="hypothesis_id" placeholder="hypothesis_id" v-model="hypothesis_id"/>
           <div class="row">
             <div class="col-sm-6" :class="isRTL ? 'text-right' : 'text-left'">
               <h5 class="card-category">Total shipments</h5>
@@ -17,7 +16,7 @@
                 data-toggle="buttons"
               >
                 <label
-                  v-for="(option, index) in bigLineChartCategories"
+                  v-for="(option, index) in this.bigLineChartCategories"
                   :key="option.name"
                   class="btn btn-sm btn-primary btn-simple"
                   :class="{ active: bigLineChart.activeIndex === index }"
@@ -43,96 +42,12 @@
           <line-chart
             style="height: 100%"
             ref="bigChart"
-            :chart-data="bigLineChart.chartData"
+            :chart-data="this.bigLineChart.chartData"
             :gradient-colors="bigLineChart.gradientColors"
             :gradient-stops="bigLineChart.gradientStops"
             :extra-options="bigLineChart.extraOptions"
           >
           </line-chart>
-        </div>
-      </card>
-    </div>
-
-
-    <!-- Small charts -->
-    <div class="col-lg-4" :class="{ 'text-right': isRTL }">
-      <card type="chart">
-        <template slot="header">
-          <h5 class="card-category">Total Shipments</h5>
-          <h3 class="card-title">
-            <i class="tim-icons icon-bell-55 text-primary "></i> 763,215
-          </h3>
-        </template>
-        <div class="chart-area">
-          <line-chart
-            style="height: 100%"
-            :chart-data="purpleLineChart.chartData"
-            :gradient-colors="purpleLineChart.gradientColors"
-            :gradient-stops="purpleLineChart.gradientStops"
-            :extra-options="purpleLineChart.extraOptions"
-          >
-          </line-chart>
-        </div>
-      </card>
-    </div>
-    <div class="col-lg-4" :class="{ 'text-right': isRTL }">
-      <card type="chart">
-        <template slot="header">
-          <h5 class="card-category">Daily Sales</h5>
-          <h3 class="card-title">
-            <i class="tim-icons icon-delivery-fast text-info "></i> 3,500€
-          </h3>
-        </template>
-        <div class="chart-area">
-          <bar-chart
-            style="height: 100%"
-            :chart-data="blueBarChart.chartData"
-            :gradient-stops="blueBarChart.gradientStops"
-            :extra-options="blueBarChart.extraOptions"
-          >
-          </bar-chart>
-        </div>
-      </card>
-    </div>
-    <div class="col-lg-4" :class="{ 'text-right': isRTL }">
-      <card type="chart">
-        <template slot="header">
-          <h5 class="card-category">Completed tasks</h5>
-          <h3 class="card-title">
-            <i class="tim-icons icon-send text-success "></i> 12,100K
-          </h3>
-        </template>
-        <div class="chart-area">
-          <line-chart
-            style="height: 100%"
-            :chart-data="greenLineChart.chartData"
-            :gradient-stops="greenLineChart.gradientStops"
-            :extra-options="greenLineChart.extraOptions"
-          >
-          </line-chart>
-        </div>
-      </card>
-    </div>
-    <div class="col-lg-5">
-      <card type="tasks" :header-classes="{ 'text-right': isRTL }">
-        <template slot="header" class="d-inline">
-          <h6 class="title d-inline">Tasks (5)</h6>
-          <p class="card-category d-inline">Today</p>
-
-          <base-dropdown
-            menu-on-right=""
-            tag="div"
-            title-classes="btn btn-link btn-icon"
-            class="float-right"
-          >
-            <i slot="title" class="tim-icons icon-settings-gear-63"></i>
-            <a class="dropdown-item" href="#pablo"> Action </a>
-            <a class="dropdown-item" href="#pablo"> Another action </a>
-            <a class="dropdown-item" href="#pablo"> Something else </a>
-          </base-dropdown>
-        </template>
-        <div class="table-full-width table-responsive">
-          <task-list></task-list>
         </div>
       </card>
     </div>
@@ -179,60 +94,29 @@ import TaskList from '@/components/Dashboard/TaskList';
 import config from '@/config';
 import { Table, TableColumn } from 'element-ui';
 
-let bigChartData = [
-  [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-  [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-  [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
-]
-let bigChartLabels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-let bigChartDatasetOptions = {
-  fill: true,
-  borderColor: config.colors.primary,
-  borderWidth: 2,
-  borderDash: [],
-  borderDashOffset: 0.0,
-  pointBackgroundColor: config.colors.primary,
-  pointBorderColor: 'rgba(255,255,255,0)',
-  pointHoverBackgroundColor: config.colors.primary,
-  pointBorderWidth: 20,
-  pointHoverRadius: 4,
-  pointHoverBorderWidth: 15,
-  pointRadius: 4,
-}
 
 export default {
   async fetch() {
+    const preferredHypothesisOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: this.hypothesis_id })
+    };
+    this.tableData = await fetch("http://localhost:8000/strategy/preferred_hypothesis", preferredHypothesisOptions).then(r => r.json());
 
-    const requestOptions = {
+    const hypothesisdataOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id: this.hypothethis_id })
+      body: JSON.stringify({ id: this.user_id })
     };
-    const response = await fetch("http://localhost:8000/strategy/preferred_hypothethis", requestOptions).then(res => res.json());
-    //const data = await response.json();
-    this.tableData = response
-    console.log(response)
-    // POST request using fetch with async/await
-    /**
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({id: 2})
-    };
-    const response = await fetch("http://localhost:8000/strategy/preferred_hypothethis")//, requestOptions);
-    const data = await response.json();
-    this.tableData = data
-    console.log(response)
-    */
-    // this.postId = data.id;
-    // let t = await this.$http.$post('http://localhost:8000/strategy/preferred_hypothethis', payload)
-    // this.tableData = t.json()
-    //var asObject = JSON.parse(tableDat);
-    //console.log(typeof(this.asObject))
-    //console.log(typeof(tableDat))
-    // this.goals_list = await fetch('http://localhost:8000/strategy/goals').then(res => res.json())
-    // let r = await fetch('http://localhost:8000/strategy/coins_list').then(res => res.json())
-    // this.options = r.map(el=>el.symbol)
+    const hypothesisData = await fetch("http://localhost:8000/strategy/hypothesis_data", hypothesisdataOptions).then(r => r.json());
+
+    this.bigChartLabels = hypothesisData["bigChartLabels"];
+    var bigChartData = []
+    for (const [key, value] of Object.entries(hypothesisData["bigChartData"])) {bigChartData.push(value);}
+    this.bigChartData = bigChartData;
+
+    console.log("FINI LE JAVASCRIPT");
   },
   fetchOnServer: false,
   name: 'dashboard',
@@ -245,159 +129,65 @@ export default {
   },
   data () {
     return {
-      hypothethis_id: 2,
       tableData: [],
-      /*
-      tableData: [
-        {
-          id: 1,
-          name: 'Dakota Rice',
-          salary: '$36.738',
-          country: 'Niger',
-          city: 'Oud-Turnhout'
-        },
-        {
-          id: 2,
-          name: 'Minerva Hooper',
-          salary: '$23,789',
-          country: 'Curaçao',
-          city: 'Sinaai-Waas'
-        },
-        {
-          id: 3,
-          name: 'Sage Rodriguez',
-          salary: '$56,142',
-          country: 'Netherlands',
-          city: 'Baileux'
-        },
-        {
-          id: 4,
-          name: 'Philip Chaney',
-          salary: '$38,735',
-          country: 'Korea, South',
-          city: 'Overland Park'
-        },
-        {
-          id: 5,
-          name: 'Doris Greene',
-          salary: '$63,542',
-          country: 'Malawi',
-          city: 'Feldkirchen in Kärnten'
-        }
-      ],*/
-      bigLineChart: {
+      bigChartData: {},
+      bigChartLabels: [],
+      hypothesis_id: 1,
+      user_id: 1,
+      bigLineChartCategories: [
+        {name: 'mimimim', icon: 'tim-icons icon-single-02'}, 
+        {name: 'Truc', icon: 'tim-icons icon-gift-2'},
+        {name: 'Mich', icon: 'tim-icons icon-tap-02'}
+      ],
+      bigChartDatasetOptions: {
+        fill: true,
+        borderColor: config.colors.primary,
+        borderWidth: 2,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        pointBackgroundColor: config.colors.primary,
+        pointBorderColor: 'rgba(255,255,255,0)',
+        pointHoverBackgroundColor: config.colors.primary,
+        pointBorderWidth: 20,
+        pointHoverRadius: 4,
+        pointHoverBorderWidth: 15,
+        pointRadius: 4,
+      },
+    };
+  },
+  computed: {
+    bigLineChart () {
+      return {
         activeIndex: 0,
         chartData: {
           datasets: [{
-            ...bigChartDatasetOptions,
-            data: bigChartData[0]
+            ...this.bigChartDatasetOptions,
+            data: this.bigChartData[0]
           }],
-          labels: bigChartLabels
+          labels: this.bigChartLabels
         },
         extraOptions: chartConfigs.purpleChartOptions,
         gradientColors: config.colors.primaryGradient,
         gradientStops: [1, 0.4, 0],
-        categories: []
-      },
-      purpleLineChart: {
-        extraOptions: chartConfigs.purpleChartOptions,
-        chartData: {
-          labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-          datasets: [
-            {
-              label: 'Data',
-              fill: true,
-              borderColor: config.colors.primary,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.primary,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.primary,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [80, 100, 70, 80, 120, 80]
-            }
-          ]
-        },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.2, 0]
-      },
-      greenLineChart: {
-        extraOptions: chartConfigs.greenChartOptions,
-        chartData: {
-          labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-          datasets: [
-            {
-              label: 'My First dataset',
-              fill: true,
-              borderColor: config.colors.danger,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.danger,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.danger,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [90, 27, 60, 12, 80]
-            }
-          ]
-        },
-        gradientColors: [
-          'rgba(66,134,121,0.15)',
-          'rgba(66,134,121,0.0)',
-          'rgba(66,134,121,0)'
-        ],
-        gradientStops: [1, 0.4, 0]
-      },
-      blueBarChart: {
-        extraOptions: chartConfigs.barChartOptions,
-        chartData: {
-          labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-          datasets: [
-            {
-              label: 'Countries',
-              fill: true,
-              borderColor: config.colors.info,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              data: [53, 20, 10, 80, 100, 45]
-            }
-          ]
-        },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.4, 0]
-      }
-    };
-  },
-  computed: {
+        categories: this.bigLineChartCategories
+      };
+    },
     enableRTL () {
+      // Right-To-Left pour les langues qui se lisent à l'envers
       return this.$route.query.enableRTL;
     },
     isRTL () {
       return this.$rtl.isRTL;
     },
-    bigLineChartCategories () {
-      return [{ name: 'Accounts', icon: 'tim-icons icon-single-02' }, {
-        name: 'Purchases',
-        icon: 'tim-icons icon-gift-2'
-      }, { name: 'Sessions', icon: 'tim-icons icon-tap-02' }];
-    }
   },
   methods: {
     initBigChart (index) {
       let chartData = {
         datasets: [{
-          ...bigChartDatasetOptions,
-          data: bigChartData[index]
+          ...this.bigChartDatasetOptions,
+          data: this.bigChartData[index]
         }],
-        labels: bigChartLabels
+        labels: this.bigChartLabels
       };
       this.$refs.bigChart.updateGradients(chartData);
       this.bigLineChart.chartData = chartData;
@@ -405,7 +195,7 @@ export default {
     }
   },
   mounted () {
-    this.initBigChart(0);
+    this.initBigChart(1);
   }
 }
 </script>
